a35cc8a43677f8ff0d81c6f5018ac7d1e1eb165c
WillKirkhope <kirkhopetraining@yahoo.com>
2018-08-24 17:12:23 -0600

    Deployed to firebase, styling is now not correct

--- borough-fe/src/components/EventCard.vue
+++ borough-fe/src/components/EventCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="event-listitem">
   <b-card title=""
-          img-src="https://placeimg.com/400/200/nature?t=1535076287388"
+          img-src="https://picsum.photos/600/300/?image=25"
           img-alt="Image"
           img-top
           tag="article"
@@ -28,35 +28,12 @@ export default {
   fetch(`https://openvillage.herokuapp.com/events/${this.myEvents[0].id}`, {
     method: 'DELETE'
   })
-    // .then(window.location.replace("https://openvillage-8d44b.firebaseapp.com/"))
 }
 }
 }
 </script>
 
 <style>
-/* .Event-card {
-  display: flex;
-  flex-direction: column;
-  background-color:rgb(232, 245, 252);
-  color: black;
-  text-align: center;
-  text-decoration: none;
-  font-size: 1vw;
-  height: 30vw;
-  width: 30vw;
-  /* font-weight: 400;
-  margin-top: 10vw;
-  margin-bottom: 1vw;
-  padding: .5vw .5vw; */
-  /* border-radius: .25vw;
-  border: 1px solid black;
-  box-shadow: .06vw .06vw .06vw 0vw rgba(0,0,0,0.75);
-} */
-/* p {
-  font-size: 1.5vw;
-} */
-
 .event-listitem {
   display: flex;
   flex-direction: row;
